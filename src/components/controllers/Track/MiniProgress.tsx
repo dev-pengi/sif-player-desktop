b31768835396d639d1e8bcfcbb28e689d560daf3
dev-pengi <siffffdoul@gmail.com>
2024-01-29 20:11:29 +0100

    fixed mini progress not appearing on full screen bug

--- src/components/controllers/Track/MiniProgress.tsx
+++ src/components/controllers/Track/MiniProgress.tsx
@@ -16,50 +16,32 @@ const MiniProgress: FC = () => {
   }, [controllersDeps]);
   return (
     <>
-      <p
-        className="absolute"
+      <div
         style={{
-          visibility: "hidden",
+          background: "#ffffff20",
+          height: 5,
+          opacity: showMiniBar && miniProgressBar ? 1 : 0,
         }}
+        className="w-full absolute -bottom-[1px] left-0 z-[1000] duration-100"
       >
-        {String(!controllersDeps.length)}
-      </p>
-      <AnimatePresence>
-        {showMiniBar && miniProgressBar && (
-          <motion.div
-            variants={{
-              hidden: { opacity: 0 },
-              visible: { opacity: 0.8 },
-            }}
-            style={{
-              background: "#ffffff20",
-              height: 5,
-            }}
-            initial="hidden"
-            animate="visible"
-            exit="hidden" 
-            className="w-full absolute -bottom-[1px] left-0 z-[1000]"
-          >
-            <motion.div
-              className="w-full h-full absolute left-0 bottom-0 right-0"
-              style={{
-                background: primaryColor,
-                transformOrigin: "0 50%",
-              }}
-              transition={{
-                type: "tween",
-                duration: 0,
-              }}
-              initial={{
-                scaleX: 0,
-              }}
-              animate={{
-                scaleX: timePercentage,
-              }}
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
+        <motion.div
+          className="w-full h-full absolute left-0 bottom-0 right-0"
+          style={{
+            background: primaryColor,
+            transformOrigin: "0 50%",
+          }}
+          transition={{
+            type: "tween",
+            duration: 0,
+          }}
+          initial={{
+            scaleX: 0,
+          }}
+          animate={{
+            scaleX: timePercentage,
+          }}
+        />
+      </div>
     </>
   );
 };
